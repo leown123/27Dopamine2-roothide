38789577d75edef04333a7f524464ca796929b4c
leown123 <136781210+leown123@users.noreply.github.com>
2026-02-26 22:42:25 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -506,9 +506,6 @@ if (load_executable_path() == 0)
 		if (dyldInsertLibraries) 
 		{
 			unsetenv("DYLD_INSERT_LIBRARIES");
-			unsetenv("_SafeMode");
-			unsetenv("_MSSafeMode");
-			unsetenv("DISABLE_TWEAKS");
 			NSLog(@"小罪ADD: systemhook: unsetenv DYLD_INSERT_LIBRARIES success");
 		}
 
