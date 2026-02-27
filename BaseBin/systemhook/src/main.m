3955b112ce86f82e9a1fa0859b572a4bf8058e3a
leown123 <136781210+leown123@users.noreply.github.com>
2026-02-27 12:02:19 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -657,7 +657,7 @@ if (load_executable_path() == 0)
 	{
 		NSLog(@"小罪ADD: systemhook: DeltaForceClient 启动！：%s", gExecutablePath);
 
-		return;
+		//return;
 		
 		gFullyDebugged = true;
 		if (jbclient_process_checkin(&JB_RootPath, &JB_BootUUID, &JB_SandboxExtensions, &gFullyDebugged) == 0) 
