e33ba6b4b2601d335ce628079ea5ad68ee81cadb
leown123 <136781210+leown123@users.noreply.github.com>
2026-03-04 11:33:30 +0800

    Update MemoryShare.m

--- BaseBin/systemhook/src/MemoryShare.m
+++ BaseBin/systemhook/src/MemoryShare.m
@@ -18,17 +18,20 @@ size_t shareSize = 1024 * 30; // 30Kb共享空间
 void* openShareChannel(void)
 {
     NSString *tmpDir = NSTemporaryDirectory();
-    NSLog(@"小罪ADD: systemhook：拟共享的tmp directory path: %@", tmpDir);
+    NSLog(@"小罪ADD: systemhook：openShareChannel 拟共享的tmp directory path: %@", tmpDir);
         
     // 示例：构建一个临时文件路径
     NSString *fileName = @".ldata";
     NSString *filePath = [tmpDir stringByAppendingPathComponent:fileName];
-    NSLog(@"小罪ADD: systemhook：拟共享的tmp file path: %@", filePath);
+    NSLog(@"小罪ADD: systemhook：openShareChannel 拟共享的tmp file path: %@", filePath);
         
     // 可以将filePath传递给open函数使用
     // int fd = open([filePath UTF8String], O_CREAT | O_WRONLY, 0644);
 
     int fd = open([filePath UTF8String], O_RDWR | O_CREAT, 0777);
+
+    NSLog(@"小罪ADD: systemhook：openShareChannel fd: %d", fd);
+    
     if (fd < 0) {
         return (void*)-1;
     }
@@ -39,6 +42,9 @@ void* openShareChannel(void)
     // 修改用户组为mobile
     chown(sharePath, 501, 501);
     void* ptr = mmap(NULL, shareSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    NSLog(@"小罪ADD: systemhook：openShareChannel mmapptr: %d", ptr);
+    
     if (ptr == MAP_FAILED) {
         return (void*)-1;
     }
