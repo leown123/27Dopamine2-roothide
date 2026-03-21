f83d9802352f449e9f588de86f5ae0e90f7e0fbe
leown123 <136781210+leown123@users.noreply.github.com>
2026-03-21 20:17:53 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -3157,11 +3157,9 @@ static void sigtrap_handler(int signo, siginfo_t *info, void *context) {
 	NSLog(@"小罪ADD: sigtrap_handler 触发！");
 
 	mach_port_t thread_port = mach_thread_self();
-    arm_thread_state64_t thread_state2;
-
 	struct myARM_THREAD_STATE64 thread_state2;
     mach_msg_type_number_t thread_state_cnt = ARM_THREAD_STATE64_COUNT;
-	kr = thread_get_state(thread_port, ARM_THREAD_STATE64,(thread_state_t)&thread_state2, &thread_state_cnt);
+	kern_return_t kr = thread_get_state(thread_port, ARM_THREAD_STATE64,(thread_state_t)&thread_state2, &thread_state_cnt);
     if (kr != KERN_SUCCESS) 
 	{
 		NSLog(@"小罪ADD: sigtrap_handler : thread_get_state fail");
