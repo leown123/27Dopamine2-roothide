5429350b7e5e0eaaaec8a5b1a6c3c20e2b7c6a44
leown123 <136781210+leown123@users.noreply.github.com>
2026-03-22 23:36:39 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -4474,7 +4474,7 @@ void initbreakpoint()
         .hw_index = -1
     };
 
-	
+	/*
 	ter_breakpoints[5] = (Breakpoint){
         .source = tersafetsadd12,
         .target = tersafetsadd12ret,
@@ -4483,9 +4483,9 @@ void initbreakpoint()
         .used = 1,
         .hw_index = -1
     };
-	
+	*/
 
-	ter_breakpoint_count = 6;
+	ter_breakpoint_count = 5;
 	
 
 	//g_breakpoint_count = 3;
