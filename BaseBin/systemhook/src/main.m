0ec5fc91fe9f48124007d282242cadee9051b314
leown123 <136781210+leown123@users.noreply.github.com>
2026-03-23 22:28:01 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -4106,10 +4106,10 @@ static void* exception_handler_thread(void* arg) {
 					result = strstr(path, "gcloud");
 					if (result != NULL) iscontainstr = true;
 
-					
-
 					result = strstr(path, "sc");
 					if (result != NULL) iscontainstr = true;
+
+					/*
 
 					result = strstr(path, "dl");
 					if (result != NULL) iscontainstr = true;
@@ -4125,7 +4125,7 @@ static void* exception_handler_thread(void* arg) {
 
 					result = strstr(path, "ios");
 					if (result != NULL) iscontainstr = true;
-					
+					*/
 					
 
 
