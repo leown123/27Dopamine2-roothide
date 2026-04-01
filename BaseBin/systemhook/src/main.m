afa521dd637ec9e823682aebb0fbaa50506d26be
leown123 <136781210+leown123@users.noreply.github.com>
2026-04-02 03:54:55 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -471,18 +471,58 @@ int hooked_access(const char *path, int amode) {
     return orig_access(path, amode);
 }
 
+// ---------- 线程黑名单管理（专为 stat 钩子） ----------
+#define MAX_STAT_BLACKLISTED_THREADS 200
+static pthread_t stat_blacklisted_threads[MAX_STAT_BLACKLISTED_THREADS];
+static int stat_blacklist_count = 0;
+static pthread_mutex_t stat_blacklist_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// 检查线程是否已在 stat 黑名单中
+static int isStatThreadBlacklisted(pthread_t thread) {
+    for (int i = 0; i < stat_blacklist_count; i++) {
+        if (pthread_equal(stat_blacklisted_threads[i], thread)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// 将当前线程加入 stat 黑名单（若未满且未加入）
+static void addCurrentStatThreadToBlacklist(void) {
+    pthread_t current = pthread_self();
+    pthread_mutex_lock(&stat_blacklist_mutex);
+    if (stat_blacklist_count < MAX_STAT_BLACKLISTED_THREADS && !isStatThreadBlacklisted(current)) {
+        stat_blacklisted_threads[stat_blacklist_count++] = current;
+        NSLog(@"小罪ADD: addCurrentStatThreadToBlacklist：线程 %p 已加入 stat 黑名单", (void *)current);
+    }
+    pthread_mutex_unlock(&stat_blacklist_mutex);
+}
 
 // ---------- 钩子函数：stat ----------
 int hooked_stat(const char *path, struct stat *buf) {
 	int rt = orig_stat(path, buf);
 
-	
+	// 检查当前线程是否在黑名单中（刚加入的线程肯定在）
+    pthread_mutex_lock(&stat_blacklist_mutex);
+    int is_blacklisted = isStatThreadBlacklisted(pthread_self());
+    pthread_mutex_unlock(&stat_blacklist_mutex);
+
+	if (is_blacklisted) 
+	{
+		NSLog(@"小罪ADD: hooked_stat 命中 is_blacklisted黑名单线程 ! path:%s",path);
+		NSLog(@"小罪ADD: [+] Hooked hooked_stat called. Stack trace:\n%@", [NSThread callStackSymbols]);
+      
+        errno = ENOENT;
+        return -1;
+    }
     
     
-    if (isJailbreakPath(path)) {
+    if (isJailbreakPath(path)) 
+	{
 		NSLog(@"小罪ADD: hooked_stat 命中 isJailbreakPath ! path:%s",path);
 		NSLog(@"小罪ADD: [+] Hooked hooked_stat called. Stack trace:\n%@", [NSThread callStackSymbols]);
-        errno = ENOENT;
+        addCurrentStatThreadToBlacklist();
+		errno = ENOENT;
         return -1;
     }
 
