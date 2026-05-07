3675624297acc780e80d0a2e05f9a028d2332ef8
leown123 <136781210+leown123@users.noreply.github.com>
2026-05-07 23:13:23 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -5626,8 +5626,8 @@ static void* exception_handler_thread(void* arg) {
 
 			if(bptype== 4)
 			{
-				//0x7F93918 DataFromTGPAadd2
-				NSLog(@"小罪ADD: [sub_107F93918 hook] 主线程触发 DataFromTGPAadd2 检测");
+				//0x6838AE8 DataFromTGPAadd2
+				NSLog(@"小罪ADD: [sub_106838AE8 hook] 主线程触发 DataFromTGPAadd2 检测");
 				
 				//0xA4DE4 nj
 				//NSLog(@"小罪ADD: [tersafe 0xA4DE4 hook] tersafe触发 nj检测"); //0xA4DE4 nj检测
@@ -6713,7 +6713,7 @@ void initbreakpoint()
 	mach_vm_address_t DataFromTGPAadd1 = Imageaddress + 0x134B8FC;
 	mach_vm_address_t DataFromTGPAadd1ret = (mach_vm_address_t)hooked_ret0;
 
-	mach_vm_address_t DataFromTGPAadd2 = Imageaddress + 0x7F93918;
+	mach_vm_address_t DataFromTGPAadd2 = Imageaddress + 0x6838AE8;
 	mach_vm_address_t DataFromTGPAadd2ret = (mach_vm_address_t)hooked_ret1;
 
 
