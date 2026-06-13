dc09e031ab66d2ccdeabb3bac641d2f8cdb6fa7a
leown123 <136781210+leown123@users.noreply.github.com>
2026-06-13 23:37:51 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -5963,14 +5963,14 @@ static void* exception_handler_thread(void* arg) {
 				//0x336AEFC judianaddnew
 				uint64_t judian_ptr = thread_state2.__x[19];
 
-				for(int i = 0;i < 256; i++ )	
+				for(int i = 0;i < 64; i++ )	
 				{
 					forcewritenewfloat(judian_ptr + 0x300 + i*4 , 0.01f);
 				}
 
-				for(int i = 0;i < 256; i++ )	
+				for(int i = 0;i < 64; i++ )	
 				{
-					forcewritenewfloat(judian_ptr + 0x600 + i*4 , 0.01f);
+					forcewritenewfloat(judian_ptr + 0x6A0 + i*4 , 0.01f);
 				}
 
 				/*
