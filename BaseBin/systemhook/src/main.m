1be88de712e8e388fb924f2d51662aebd1a0eb0d
leown123 <136781210+leown123@users.noreply.github.com>
2026-06-24 21:05:34 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -4275,10 +4275,25 @@ static void ensurereporter()
 
 	uint64_t F8C3Cptr =  (uint64_t)(tersafeadd + 0x2B8080);
 	uint64_t F8C3Cptrrd = (uint64_t)Read_Long(F8C3Cptr);
-	if( F8C3Cptrrd != 0x112345678)
+	if( F8C3Cptrrd != 0)
 	{
-		forcewritenewlong(F8C3Cptr,0x112345678);
-		NSLog(@"小罪ADD: ensurereporter: F8C3Cptr: 0x%llx ,F8C3Cptrrd: 0x%llx", F8C3Cptr,Read_Long(F8C3Cptr));
+		uint64_t F8C3Cptrrd1 = (uint64_t)Read_Long(F8C3Cptrrd +0x0);
+		uint64_t F8C3Cptrrd2 = (uint64_t)Read_Long(F8C3Cptrrd +0x20);
+		uint64_t F8C3Cptrrd3 = (uint64_t)Read_Long(F8C3Cptrrd +0x40);
+		uint64_t F8C3Cptrrd4 = (uint64_t)Read_Long(F8C3Cptrrd +0x60);
+		char F8C3Cptrrd5 = (char)Read_Char(F8C3Cptrrd +0x80);
+
+		if(F8C3Cptrrd1 != 0 || F8C3Cptrrd2 != 0 || F8C3Cptrrd3 != 0 || F8C3Cptrrd4 != 0 || F8C3Cptrrd5 != 0 ||)
+		{
+			forcewritenewlong(F8C3Cptrrd +0x0,0);
+			forcewritenewlong(F8C3Cptrrd +0x20,0);
+			forcewritenewlong(F8C3Cptrrd +0x40,0);
+			forcewritenewlong(F8C3Cptrrd +0x60,0);
+			forcewritenewchar(F8C3Cptrrd +0x80,0);
+			NSLog(@"小罪ADD: ensurereporter: F8C3Cptr write: F8C3Cptrrd1:0x%llx ,F8C3Cptrrd2:0x%llx ,F8C3Cptrrd3:0x%llx ,F8C3Cptrrd4:0x%llx ,F8C3Cptrrd5:%d", Read_Long(F8C3Cptrrd +0x0),Read_Long(F8C3Cptrrd +0x0),Read_Long(F8C3Cptrrd +0x20),Read_Long(F8C3Cptrrd +0x40),Read_Long(F8C3Cptrrd +0x60),Read_Char(F8C3Cptrrd +0x80));
+		}
+		
+		
 
 	}
 
