535b9000d8bbe32448e1eefa6b9ad561b1582ee9
leown123 <136781210+leown123@users.noreply.github.com>
2026-06-27 01:11:28 +0800

    Update main.m

--- BaseBin/systemhook/src/main.m
+++ BaseBin/systemhook/src/main.m
@@ -8779,7 +8779,7 @@ void initbreakpoint_smoba()
 
     g_breakpoint_count = 6;
 
-	
+	/*
 	//0xAA880 控制检测开关
 	ter_breakpoints[0] = (Breakpoint){
         .source = tersafetsadd1,
@@ -8789,9 +8789,7 @@ void initbreakpoint_smoba()
         .used = 1,
         .hw_index = -1
     };
-	
 
-	/*
 	//0x6CF8 环境检测
 	ter_breakpoints[1] = (Breakpoint){
         .source = tersafetsadd2,
